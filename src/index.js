d7d8f0fe6e6b2b34f761a8e991cc48776cdd4e49
Shadman <shadman.alizada@gmail.com>
2023-08-15 22:49:10 +0400

    styles: used module scss instead of css

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
 import App from './App';
-import './index.scss';
-import './media.scss';
+import './global.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
