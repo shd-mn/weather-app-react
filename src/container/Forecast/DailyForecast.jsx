28d519582f90e66a43cee2b1431525cf5db27455
Shadman <shadman.alizada@gmail.com>
2023-08-18 23:33:23 +0400

    refactor: moved DailyForecast detailed forecast info to Info comp, added hover state

--- src/container/Forecast/DailyForecast.jsx
+++ src/container/Forecast/DailyForecast.jsx
@@ -1,46 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import moment from 'moment';
 import styles from './dailyForecast.module.scss';
 import Icon from '../../icons';
 import { weatherIcons } from '../../data/weatherIcons';
-const DailyForecast = ({ weather }) => {
+import Info from '../../components/common/Info';
+const DailyForecast = ({ day, index }) => {
+    const [isHovered, setIsHovered] = useState(false);
     return (
-        <>
-            {weather.daily.map((day, index) => (
-                <article key={day.dt} className={styles['daily-forecast']}>
-                    <p className={styles.time}>
-                        {index === 0
-                            ? 'Today'
-                            : moment(day.dt * 1000).format('DD MMM')}
-                    </p>
-                    <h2 className="heading-5">
-                        {day.temp.day.toFixed(0)}° / {day.temp.night.toFixed(0)}
-                        °
-                    </h2>
-                    <Icon icon={weatherIcons[day.weather[0].main]} size="2.8rem" />
-                    <div className={styles['forecast-info']}>
-                        <p className={styles['feels-like']}>
-                            Feels like {day.feels_like.day.toFixed(0)}°C
-                        </p>
-                        <p className={styles.info}>
-                            <Icon icon="wind-speed" size="2.2rem" />
-                            {day.wind_speed} m/s
-                        </p>
-                        <p className={styles.info}>
-                            <Icon icon="cloud" size="2.2rem" /> {day.clouds} %
-                        </p>
-                        <p className={styles.info}>
-                            <Icon icon="pressure" size="2.2rem" />
-                            {day.pressure} PS
-                        </p>
-                        <p className={styles.info}>
-                            <Icon icon="humidity" size="2.2rem" />
-                            {day.humidity} %
-                        </p>
-                    </div>
-                </article>
-            ))}
-        </>
+        <article
+            className={styles['daily-forecast']}
+            onMouseOver={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+        >
+            {/* Time */}
+            <p className={styles.time}>
+                {index === 0 ? 'Today' : moment(day.dt * 1000).format('DD MMM')}
+            </p>
+
+            {/* Temp */}
+            <h2 className="heading-5">
+                {day.temp.day.toFixed(0)}° / {day.temp.night.toFixed(0)}°
+            </h2>
+            <Icon icon={weatherIcons[day.weather[0].main]} size="2.8rem" />
+
+            {/* Info */}
+            <Info data={day} isHovered={isHovered} />
+        </article>
     );
 };
 
